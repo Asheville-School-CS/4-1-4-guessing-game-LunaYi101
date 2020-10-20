4076b4097c629b368fd7eabc5228de522c5ce446
jjoon137 <kangj21@ashevilleschool.org>
2020-10-20 08:49:47 -0400

    Submission 1

--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -10,18 +10,28 @@ public class Main
 
         // Ask the user to guess a number from 0 to 100
         // Get the first guess using scan.nextInt();
-
+        System.out.println("Guess a number from 0 to 100");
+        int guess = scan.nextInt();
+        int answer = (int)(Math.random() * 100);
 
         // Loop while the guess does not equal the random number,
+        while(guess != answer) {
 
-        // If the guess is less than the random number, print out "Too low!"
+            // If the guess is less than the random number, print out "Too low!"
+            if(guess < answer){
+                System.out.println("Too low!");
+            }
 
-        // If the guess is greater than the random number, print out "Too high!"
+            // If the guess is greater than the random number, print out "Too high!"
+            if(guess > answer){
+                System.out.println("Too high!");
+            }
 
-        // Get a new guess (save it into the same variable)
-
+            // Get a new guess (save it into the same variable)
+            System.out.print("Guess?: ");
+            guess = scan.nextInt();
+        }
         // Print out "You got it!"
-
-
+        System.out.println("You got it!");
     }
 }
